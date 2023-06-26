b709da6d0bafb213800ee8f167d26a51578dcf98
aayushisolanki28 <36412316+aayushisolanki28@users.noreply.github.com>
2023-06-26 12:35:46 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,38 @@
-import * as React from 'react';
-import './style.css';
+import React, { useState } from 'react';
 
-export default function App() {
+function App() {
+  const fruits = [
+    'Banana',
+    'Apple',
+    'Orange',
+    'Mango',
+    'Pineapple',
+    'Watermelon',
+  ];
+  const [filterList, setFilterList] = useState(fruits);
+  const handleSearch  = (event) =>{
+   if(event.target.value == ""){
+    setFilterList(fruits);
+    return;
+   }
+   const filteredValues = fruits.filter(item =>
+     item.toLowerCase().indexOf(event.target.value.toLowerCase()) != -1)
+     setFilterList(filteredValues)
+   
+   
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <div>
+        <label>Search Item: </label>
+        <input type="text" name="search" placeholder="Search Here..." onChange = {handleSearch} />
+      </div>
+      {filterList &&
+        filterList.map((item, index) => (
+          <li key={index}>{item}</li> 
+        ))}
     </div>
   );
 }
+
+export default App;
